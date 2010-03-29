3373d041c97847ef090f1315e19d3bed412eb8a0
curtis-johnson <curtis-johnson@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-03-29 01:54:44 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@84 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/assembler/CacheMoney/src/Instructions/Immediate/cBranchEqual.java
+++ trunk/assembler/CacheMoney/src/Instructions/Immediate/cBranchEqual.java
@@ -1,8 +1,6 @@
 package Instructions.Immediate;
 
 import Instructions.cImmediateInstruction;
-import Interfaces.IImmediateInstruction;
-import Interfaces.IInstruction;
 
 public class cBranchEqual extends cImmediateInstruction {
 
