6a8332523c3c26b996b8d66bac4cf72675829cef
curtis-johnson <curtis-johnson@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-03-29 06:32:53 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@86 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/assembler/CacheMoney/src/Instructions/Arithmetic/cNoOperation.java
+++ trunk/assembler/CacheMoney/src/Instructions/Arithmetic/cNoOperation.java
@@ -8,4 +8,27 @@ public class cNoOperation extends cAdd {
 		return "nop";
 	}
 	
+	/**
+	 *	NOP has a destination register of $0.
+	 */
+	@Override
+	public int getDestRegister() {
+		return 0;
+	}
+	
+	/**
+	 * NOP has source registers of $0.
+	 */
+	@Override
+	public int getSourceRegister1() {
+		return 0;
+	}
+	
+	/**
+	 * NOP has source registers of $0.
+	 */
+	@Override
+	public int getSourceRegister2() {
+		return 0;
+	}
 }
