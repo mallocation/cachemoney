dfc5f9bed67d7419435bc105709f5e095c9862b7
curtis-johnson <curtis-johnson@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-04-03 16:47:54 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@95 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/assembler/CacheMoney/src/Interfaces/IInstructionConstants.java
+++ trunk/assembler/CacheMoney/src/Interfaces/IInstructionConstants.java
@@ -13,10 +13,10 @@ public interface IInstructionConstants {
 	
 	/** Immediate Instructions **/
 	public static final IInstruction ADDI = new AddImmediate();
-	public static final IInstruction BEQ = new cBranchEqual();
-	public static final IInstruction BNEQ = new cBranchNotEqual();
-	public static final IInstruction LW = new cLoadWord();
-	public static final IInstruction SW = new cStoreWord();
+	public static final IInstruction BEQ = new BranchEqual();
+	public static final IInstruction BNEQ = new BranchNotEqual();
+	public static final IInstruction LW = new LoadWord();
+	public static final IInstruction SW = new StoreWord();
 	
 	/** Jump Instructions **/
 	public static final IInstruction JMP = new cJump();
