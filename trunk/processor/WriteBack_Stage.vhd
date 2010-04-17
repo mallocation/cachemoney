cb5115e3ea12a98df945aa03c4e38991c441e5cd
curtis-johnson <curtis-johnson@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-04-17 03:49:00 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@134 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/processor/WriteBack_Stage.vhd
+++ trunk/processor/WriteBack_Stage.vhd
@@ -1,9 +1,10 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY WriteBack_Stage IS
-	PORT (
+entity WriteBack_Stage is
+	port (			
 			DataMemory 	:	in std_logic_vector(31 downto 0);
 			DestReg		:	in std_logic_vector(4 downto 0);
+			
 			
 END Register32bit;
