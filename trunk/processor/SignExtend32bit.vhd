b66bb51029096c5ab79315ece89f3fa7d644f2b9
brett-baumert <brett-baumert@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-04-17 06:05:09 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@144 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/processor/SignExtend32bit.vhd
+++ trunk/processor/SignExtend32bit.vhd
@@ -2,12 +2,16 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY SignExtend32bit IS
-	PORT (		data_in		:	IN	STD_LOGIC_VECTOR(15 DOWNTO 0);
+	generic
+	(
+		DATA_WIDTH : natural := 28
+	);
+	PORT (		data_in		:	IN	STD_LOGIC_VECTOR(DATA_WIDTH-1 DOWNTO 0);
 					data_out	:	OUT	STD_LOGIC_VECTOR(31 DOWNTO 0));
 END SignExtend32bit;
 
 ARCHITECTURE Behavior OF SignExtend32bit IS
 BEGIN	
-	data_out(15 downto 0) <= data_in;
-	data_out(31 downto 16) <= (31 downto 16 => data_in(15));
+	data_out(DATA_WIDTH-1 downto 0) <= data_in;
+	data_out(31 downto DATA_WIDTH) <= (31 downto DATA_WIDTH => data_in(DATA_WIDTH-1));
 END Behavior;
