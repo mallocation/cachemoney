c2e788c0e2edb9a8da840fffbad36b739eea1f3d
curtis-johnson <curtis-johnson@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-05-01 21:56:18 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@180 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/processor/ALU32bit.vhd
+++ trunk/processor/ALU32bit.vhd
@@ -14,34 +14,21 @@ entity ALU32bit is
 end ALU32bit;
 
 architecture STRUCTURE of ALU32bit is
-	--signed adder in work library
-	component signed_adder
-		port(a,b: in std_logic_vector(31 downto 0);
-			 result: out std_logic_vector(31 downto 0);
-			 overflow: out std_logic);
-	end component;
-	--signed_multiply in work library
-	component signed_multiply
-		port(a,b: in std_logic_vector(15 downto 0);
-			 result: out std_logic_vector(31 downto 0));
-	end component;
 	signal add_overflow : std_logic;
 	signal add_result, mult_result : std_logic_vector(31 downto 0);
 begin
 	--instantiate a signed_adder
-	adder1: signed_adder
-		port map (a, b, add_result, add_overflow);
+	adder1: work.signed_adder generic map (DATA_WIDTH=>32) port map (a, b, add_result, add_overflow);
+	
 	--instantiate a signed_multiply
-	mult1: signed_multiply
-		port map (a(15 downto 0), b(15 downto 0), mult_result);
+	multiply1: work.signed_multiply generic map (DATA_WIDTH=>16) port map (a(15 downto 0), b(15 downto 0), mult_result);
 
-	--process fires on both operands, as well as the operation type
-	process(a,b,op)
+	process(add_result, mult_result, add_overflow, op)
 	begin
 		if op = '0' then --op 0 is an add operation
 			result <= add_result;
 			overflow <= add_overflow;
-		elsif op = '1' then	-- op 1 is a multiply operation
+		else -- op 1 is a multiply operation
 			result <= mult_result;
 			overflow <= '0'; -- overflow isn't possible in the multipler
 		end if;
