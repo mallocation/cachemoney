468ce69bd4726b75b4e5b74f58c1d825ef243535
curtis-johnson <curtis-johnson@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-05-01 18:20:15 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@179 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/processor/ForwardingUnit.vhd
+++ trunk/processor/ForwardingUnit.vhd
@@ -21,18 +21,24 @@ entity ForwardingUnit is
 
 		-- Source Register 2 from ALU
 		RegSrc2_FromEXStage		:	in std_logic_vector(4 downto 0);
+		
+		-- Source Register for Store Word
+		RegSrc_ForStoreWord		:	in std_logic_vector(4 downto 0);
 
 		-- Forwarding Signal to the 1st operand of the ALU
 		ForwardA : out std_logic_vector(1 downto 0);
 		
 		-- Forwarding Signal to the 2nd operand of the ALU
-		ForwardB : out std_logic_vector(1 downto 0));
+		ForwardB : out std_logic_vector(1 downto 0);
+		
+		-- Forwarding Signal to the Store Word MUX
+		ForwardC : out std_logic_vector(1 downto 0));
 		
 end ForwardingUnit;
 
 architecture STRUCTURE of ForwardingUnit is
 begin
-	process(RegWrite_FromMEMStage, RegWrite_FromWBStage, RegDest_FromMEMStage, RegDest_FromWBStage, RegSrc1_FromEXStage, RegSrc2_FromEXStage)
+	process(RegWrite_FromMEMStage, RegWrite_FromWBStage, RegDest_FromMEMStage, RegDest_FromWBStage, RegSrc1_FromEXStage, RegSrc2_FromEXStage, RegSrc_ForStoreWord)
 	begin
 	
 		--------------------------
@@ -66,6 +72,18 @@ begin
 		-- Otherwise, there is no forwarding, so get the value from the register file
 		else
 			ForwardB <= "00";
+		end if;
+		
+		--------------------------
+		-- Store Word Source Register
+		--------------------------
+		if RegWrite_FromMEMStage = '1' and RegDest_FromMEMStage /= "00000" and RegDest_FromMEMStage = RegSrc_ForStoreWord then
+			ForwardC <= "10";
+		--elsif RegWrite_FromWBStage = '1' and RegDest_FromWBStage /= "00000" and RegDest_FromMEMStage /= RegSrc_ForStoreWord and RegDest_FromWBStage = RegSrc_ForStoreWord then			
+		elsif RegWrite_FromWBStage = '1' and RegDest_FromWBStage /= "00000" and RegDest_FromWBStage = RegSrc_ForStoreWord then			
+			ForwardC <= "01";
+		else
+			ForwardC <= "00";
 		end if;
 		
 	end process;
--- trunk/testprograms/multiplyarrays_store_read.asm
+++ trunk/testprograms/multiplyarrays_store_read.asm
@@ -10,7 +10,6 @@ addi $29, $0, 0 #clear sum
 
 loop:
 	lw $5, $1
-	nop
 	lw $6, $2
 	nop
 	mult $7, $5, $6
@@ -22,7 +21,7 @@ loop:
 	addi $30, $30, 1
 	bneq $30, $4, loop
 	
-addi $3, $3, -2
+addi $3, $0, array3
 addi $30, $0, 0
 addi $29, $0, 0
 loop2:
@@ -30,18 +29,16 @@ loop2:
 	nop
 	add $29, $29, $7
 	addi $3, $3, 1
-	addi $30, $30, 1
-	bneq $30, $4, loop2
+	nop
+	lw $7, $3
+	nop
+	add $29, $29, $7
 	
+#	addi $30, $30, 1
+#	bneq $30, $4, loop2
 	
-addi $31, $29, 0
+add $31, $29, $0
 exit: jmp exit
-	
-
-
-
-	
-
 
 	.data
 length: .integer 2
