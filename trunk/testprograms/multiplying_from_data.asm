c8d51a136e999435bc42253f8ebb02a06274511d
jake-reisdorff <jake-reisdorff@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-05-01 07:16:18 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@176 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/testprograms/multiplying_from_data.asm
+++ trunk/testprograms/multiplying_from_data.asm
@@ -9,25 +9,22 @@ loop:
 beq $6, $1, loopthrough
 lw $4, $2 #load value at base of matrix1 into r4
 lw $5, $3 #load value at base of matrix2 into r5
-addi $2, $2, 1
-addi $3, $3, 1  #loop incrementations used as nops
 nop
 mult $7, $4, $5
-addi $6, $6, 1
-nop
-nop
 sw $7, $10
+addi $2, $2, 1
+addi $3, $3, 1  #loop incrementations used as nops
+addi $6, $6, 1
 addi $10, $10, 1  #loop incrementations used as nops
 jmp loop
 
-loopthrough: addi $10, $0, matrix3 #load the base of matrix3
-	nop
+        addi $10, $0, matrix3 #load the base of matrix3
 loop2:	beq $11, $1, showresults
 	lw $12, $10
-	addi $10, $10, 1
-	addi $11, $11, 1  #loop incrementations used as nops
 	nop
 	add $13, $13, $12
+	addi $10, $10, 1
+	addi $11, $11, 1  
 	jmp loop2
 
 
@@ -38,5 +35,5 @@ exit: jmp exit
 .data
 dimension: .integer 4
 matrix1: .array 1,2,3,4
-matrix2: .array 6,7,8,9
+matrix2: .array 15,16,17,18
 matrix3: .array 0,0,0,0
