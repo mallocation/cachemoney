593c454334750389f0db3611bf1fafaf45ba5e13
curtis-johnson <curtis-johnson@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-05-02 04:44:26 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@186 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/testprograms/multiplyarrays_store_read.asm
+++ trunk/testprograms/multiplyarrays_store_read.asm
@@ -4,12 +4,3 @@ addi $5, $0, -256342
 addi $5, $5, 2
 add $7, $3, $5
 add $31, $7, $0
-
-
-exit: jmp exit
-
-	.data
-length: .integer 2
-array1: .array 1,2,-523281
-array2: .array 15,16
-array3: .integer 0
