39a87ee702f304dfc0d32ed9381bdd8feed3e56e
jake-reisdorff <jake-reisdorff@bcaa0796-ab92-4845-b78f-7ce167744df0>
2010-05-02 16:47:11 +0000

    git-svn-id: https://cjfileserver/svn/cachemoney@190 bcaa0796-ab92-4845-b78f-7ce167744df0

--- trunk/testprograms/multiplying_from_data.asm
+++ trunk/testprograms/multiplying_from_data.asm
@@ -1,32 +1,33 @@
 #register 8 holds teh value
 
-lw $1, dimension #load the dimension
+lw $23, dimension #load the dimension
 nop
 addi $2, $0, matrix1 #load base of matrix1
 addi $3, $0, matrix2 #load base of matrix2
 addi $10, $0, matrix3 #load base of matrix3
+addi $1, $0, 1 #keep 1 in a register
 
 loop:
-beq $6, $1, loopthrough
+beq $6, $23, loopthrough
 lw $4, $2 #load value at base of matrix1 into r4
 nop
 lw $5, $3 #load value at base of matrix2 into r5
 nop
 mult $7, $4, $5
 sw $7, $10
-addi $2, $2, 1
-addi $3, $3, 1  #loop incrementations used as nops
-addi $6, $6, 1
-addi $10, $10, 1  #loop incrementations used as nops
+add $2, $2, $1
+add $3, $3, $1  #loop incrementations used as nops
+add $6, $6, $1
+add $10, $10, $1  #loop incrementations used as nops
 jmp loop
 
 loopthrough: addi $10, $0, matrix3 #load the base of matrix3
-loop2:	beq $11, $1, showresults
+loop2:	beq $11, $23, showresults
 	lw $12, $10
 	nop
 	add $13, $13, $12
-	addi $10, $10, 1
-	addi $11, $11, 1  
+	add $10, $10, $1
+	add $11, $11, $1  
 	jmp loop2
 
 
